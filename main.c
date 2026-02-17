0eccdfd0b5febeccda36d65bf569279297bf70e9
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-02-18 00:05:34 +0800

    Added function addrinfo

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
-// first program learn the ip
+struct addrinfo{
+    int             ai_flags;
+    int             ai_family;
+    int             ai_socktype;
+    int             ai_protocol;
+    socklen_t       ai_addrlen;
+    struct sockaddr *ai_addr;
+    char            *ai_canonname;
+
+    struct addrinfo *ai_next;
+};
