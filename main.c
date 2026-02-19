3a2880e0d980201e4b564edbb55e00a73cffc89d
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-02-20 02:42:52 +0800

    Adding struct soccadr_in

--- main.c
+++ main.c
@@ -16,3 +16,10 @@ struct sockaddr {
     unsigned short  sa_family;
     char            sa_data[14];
 };
+
+struct soccadr_in {
+    short int           sin_family;
+    unsigned short int  sin_port;
+    struct in_addr      sin_addr;
+    unsigned char       sin_zero[8];
+}
