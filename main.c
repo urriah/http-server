57e70a6aa512812c34987274a66b998a23eb4a0c
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-02-20 02:26:04 +0800

    Added struct sockaddr

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
-struct addrinfo{
+#include <stdlib.h>
+
+struct addrinfo {
     int             ai_flags;
     int             ai_family;
     int             ai_socktype;
     int             ai_protocol;
-    socklen_t       ai_addrlen;
+    size_t          ai_addrlen;
     struct sockaddr *ai_addr;
     char            *ai_canonname;
 
     struct addrinfo *ai_next;
+};
+
+struct sockaddr {
+    unsigned short  sa_family;
+    char            sa_data[14];
 };
