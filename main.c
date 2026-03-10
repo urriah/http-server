a23c52e3e90e56c874c47a9cde3f068b5fbad8cc
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-03-10 13:02:30 +0800

    Structure for IPv6 internet adress

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ struct sockaddr {
     char                sa_data[14];
 };
 
+// ipv4
 struct sockaddr_in {
     short int           sin_family;
     unsigned short int  sin_port;
@@ -27,4 +28,17 @@ struct sockaddr_in {
 
 struct in_addr {
     uint32_t    s_addr;
+};
+
+// ipv6
+struct socaddr_in6 {
+    uint16_t           sin6_family;
+    uint16_t           sin6_port;
+    uint32_t           sin6_flowinfo;
+    struct in6_addr    sin6_addr;
+    uint32_t           sin6_scope_id;
+};
+
+struct in6_addr {
+    unsigned char      s6_addr[16];
 };
