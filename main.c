2e4a8a825a9cf1c6f9d5cdc64a0c7f0a2bdd05af
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-03-12 23:43:17 +0800

    Started IP address

--- main.c
+++ main.c
@@ -51,3 +51,9 @@ struct sockaddr_storage {
     char      __ss_pad2[__SS_PAD2SIZE];
 };
 
+struct sockaddr_in sa;
+struct sockaddr_in sa6;
+
+inet_pton(AF_INET, "10.12.110.57", &(sa.sin_addr));
+inet_pton(AF_INET6, "2001:db8:63b3:1::3490", &(sa6.sin6_addr));
+
