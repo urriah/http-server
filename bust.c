afea7d0b4b8d0eca219dc287462171c657f4fd7a
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-03-17 22:10:11 +0800

    A sample call

--- bust.c
+++ bust.c
@@ -7,3 +7,18 @@ int getaddrinfo(const char *node,
                 const struct addrinfo *hints,
                 struct addrinfo **res);
 
+int status;
+struct addrinfo hints;
+struct addrinfo *servinfo;
+
+memset(&hints, 0, sizeof hints);
+hints.ai_family = AF_UNSPEC;
+hints.ai_socktype = SOCK_STREAM;
+hints.ai_flags = AI_PASSIVE;
+
+if ((status = getaddrinfo(NULL, "3490", *hints, *servinfo)) != 0) {
+  fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+  exit(1);
+}
+
+freeaddrinfo(servinfo);
