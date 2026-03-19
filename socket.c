512375308306cce39caa66b3cde3088f5bbda74c
Kirk Urriah <espina.kirkurriah@gmail.com>
2026-03-19 23:20:34 +0800

    Use the value from result

--- socket.c
+++ socket.c
@@ -2,3 +2,11 @@
 #include <sys/sockets.h>
 
 int socket(int domain, int type, int protocol);
+
+int s;
+struct addrinfo hints, *res;
+
+getaddrinfo("www.example.com", "http", &hints, &res);
+
+s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
